899e5e2cf045fc80fa1e67dd9f5632e722c81a3f
Emmanuel-compasso <emmanuel.scortega.pb@compasso.com.br>
2024-10-19 15:43:11 -0300

    meudeus

---
+++ src/app/page.jsx
@@ -0,0 +1,34 @@
+import { getProdutosDB } from "@/componentes/bd/usecases/produtoUseCases";
+import Link from "next/link";
+
+export const revalidate = 60; // tempo em segundos para revalidar
+
+export default async function Home() {
+
+  const produtos = await getProdutosDB();
+  return (
+    <div style={{ padding: '20px' }}>
+      <div className="row">
+        {produtos.length > 0 && (
+          produtos.map((produto) => (
+            <div key={produto.codigo} className="col-sm-3" >
+              <div className="card mb-3 text-center">
+                <div className="card-header">
+                  {produto.nome}
+                </div>
+                <div className="card-body ">
+                  <h5 className="card-title">{produto.nome}</h5>
+                  <p className="card-text"><small className="text-muted">Preço: {produto.valor}</small></p>
+                  <p className="card-text"><small className="text-muted">Categoria: {produto.categoria_nome}</small></p>
+                </div>
+                <div className="card-footer text-muted">
+                  <Link type="button" className="btn btn-secondary" href={`/${produto.codigo}/detalhe`}>Detalhes do produto</Link>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}

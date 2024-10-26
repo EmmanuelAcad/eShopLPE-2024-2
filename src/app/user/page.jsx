0ee85fe1626cf8d82bf055b6be6195636d9c1528
Emmanuel-compasso <emmanuel.scortega.pb@compasso.com.br>
2024-10-26 17:27:28 -0300

    amem

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,18 +1,33 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export const User = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-    const { data : session } = useSession();
+  const handleEdit = () => {
+    router.push("/user/editar");
+  };
 
-    return (
+  return (
+    <>
+      <h1>Usuário autenticado</h1>
+      {session ? (
         <>
-            <h1>Usuário autenticado</h1>
-            <h2>{JSON.stringify(session)}</h2>
+          <h2>Email: {session.user.email}</h2>
+          <h2>Nome: {session.user.name}</h2>
+          <h2>Telefone: {session.user.telefone}</h2>
+          <button onClick={handleEdit} className="btn btn-primary">
+            Editar Dados
+          </button>
         </>
-    )
-
-}
+      ) : (
+        <h2>Nenhum usuário autenticado</h2>
+      )}
+    </>
+  );
+};
 
 export default User;

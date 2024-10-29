6f9ada43a19c6f7b74a1d82d6fbcbd5019720b8d
EmmanuelAcad <111817257+EmmanuelAcad@users.noreply.github.com>
2024-10-29 10:05:13 -0300

    agrvai 2.0

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -11,13 +11,15 @@ export const User = () => {
             <h1>Usuário autenticado</h1>
             <h2>{JSON.stringify(session)}</h2>
 
-            <Link
-                className="btn btn-info"
-                title="Editar"
-                href={`/user/${session.user.email}/formulario`}
-            >
-                <i className="bi bi-pencil-square"></i>
-            </Link>
+            {session != null && (
+                <Link
+                    className="btn btn-info"
+                    title="Editar"
+                    href={`/user/${session.user.name}/formulario`}
+                >
+                    <i className="bi bi-pencil-square"></i>
+                </Link>
+            )}
         </>
     );
 };

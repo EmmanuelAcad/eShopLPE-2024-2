8935ef0f08662e7531dda2fe9eb575d62b643212
EmmanuelAcad <111817257+EmmanuelAcad@users.noreply.github.com>
2024-10-29 08:49:44 -0300

    teste user 4

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,5 +1,11 @@
 "use server";
 
+import { notFound, redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { Suspense } from 'react';
+import Loading from '@/componentes/comuns/Loading';
 
 import { autenticaUsuarioDB, atualizarUsuarioDB } from "@/componentes/bd/usecases/usuarioUseCases";
 
@@ -9,75 +15,77 @@ export default async function User() {
 
   const atualizarUsuario = async (formData) => {
 
-        const objeto = {
-            email: formData.get('email'),
-            senha: formData.get('senha'),
-            telefone: formData.get('telefone'),
-            tipo: 'U',
-            nome: formData.get('nome')
-        }
-        try {
-            await atualizarUsuarioDB(objeto);
-        } catch (err) {
-            throw new Error('Erro: ' + err);
-        }
-        revalidatePath('/user');
-        redirect('/user');
+    const objeto = {
+      email: formData.get('email'),
+      senha: formData.get('senha'),
+      telefone: formData.get('telefone'),
+      tipo: 'U',
+      nome: formData.get('nome')
     }
+    try {
+      await atualizarUsuarioDB(objeto);
+    } catch (err) {
+      throw new Error('Erro: ' + err);
+    }
+    revalidatePath('/user');
+    redirect('/user');
+  }
 
   return (
     <>
-      <h1>Usuário autenticado</h1>
+      <Suspense fallback={<Loading />}>
+        <h1>Usuário autenticado</h1>
 
-      <div defaultValue={usuario.email}></div>
+        <div defaultValue={usuario.email}></div>
 
-      <form action={atualizarUsuario}>
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-12 col-md-6">
-                              <div>
-                                    <FloatingLabel controlId="campoEmail"
-                                        label="Nome" className="mb-3">
-                                        <Form.Control type="text"
-                                            defaultValue={usuario.email} required
-                                            name="nome" />
-                                    </FloatingLabel>
-                                </div>
-                                <div>
-                                    <FloatingLabel controlId="campoSenha"
-                                        label="Senha" className="mb-3">
-                                        <Form.Control type="password"
-                                            defaultValue={usuario.senha} required
-                                            name="senha" />
-                                    </FloatingLabel>
-                                </div>
-                                <div>
-                                    <FloatingLabel controlId="campoTelefone"
-                                        label="Telefone" className="mb-3">
-                                        <Form.Control type="number"
-                                            defaultValue={usuario.telefone} required
-                                            name="telefone" />
-                                    </FloatingLabel>
-                                </div>
-                                <div>
-                                    <FloatingLabel controlId="campoNome"
-                                        label="Nome" className="mb-3">
-                                        <Form.Control type="text"
-                                            defaultValue={usuario.nome} required
-                                            name="nome" />
-                                    </FloatingLabel>
-                                </div>
-                                <div className="form-group text-center mt-3">
-                                    <button type="submit" className="btn btn-success">
-                                        Salvar <i className="bi bi-save"></i>
-                                    </button>
-                                </div>
-                            </div>
+        <form action={atualizarUsuario}>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-12 col-md-6">
+                <div>
+                  <FloatingLabel controlId="campoEmail"
+                    label="Nome" className="mb-3">
+                    <Form.Control type="text"
+                      defaultValue={usuario.email} required
+                      name="nome" />
+                  </FloatingLabel>
+                </div>
+                <div>
+                  <FloatingLabel controlId="campoSenha"
+                    label="Senha" className="mb-3">
+                    <Form.Control type="password"
+                      defaultValue={usuario.senha} required
+                      name="senha" />
+                  </FloatingLabel>
+                </div>
+                <div>
+                  <FloatingLabel controlId="campoTelefone"
+                    label="Telefone" className="mb-3">
+                    <Form.Control type="number"
+                      defaultValue={usuario.telefone} required
+                      name="telefone" />
+                  </FloatingLabel>
+                </div>
+                <div>
+                  <FloatingLabel controlId="campoNome"
+                    label="Nome" className="mb-3">
+                    <Form.Control type="text"
+                      defaultValue={usuario.nome} required
+                      name="nome" />
+                  </FloatingLabel>
+                </div>
+                <div className="form-group text-center mt-3">
+                  <button type="submit" className="btn btn-success">
+                    Salvar <i className="bi bi-save"></i>
+                  </button>
+                </div>
+              </div>
 
-                        </div>
-                    </div>
+            </div>
+          </div>
 
-                </form>
+        </form>
+      </Suspense>
     </>
   );
 };
